5aceecd86fc79524d7ae21ae1d21a5c54ecd474e
Firebear518 <phj040518@gmail.com>
2025-01-06 15:57:29 +0900

    Create guess_number_game.py

---
+++ guess_number_game.py
@@ -0,0 +1,18 @@
+from ? import ?
+from ? import ?
+
+print("----------------------------------")
+print("[#] Guess Number Game")
+print("----------------------------------")
+
+print("[#] 시작, 끝 범위를 입력해주세요.")
+start, end = map(int, input("Input: ").split())
+random_number = randomize(start, end)
+print("[#] 숫자가 생성되었습니다.")
+
+while(True):
+    ipt = 0
+    print("----------------------------------")
+    ipt = int(input("Your Input: "))
+    comparing(random_number, ipt)
+    print("----------------------------------")
